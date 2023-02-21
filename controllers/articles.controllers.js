18fbefa8743e4523b5df07fc3e37619c5faa4839
Wes <wfreemansmith@yahoo.co.uk>
2023-02-21 14:05:52 +0000

    get comments by article id added

--- controllers/articles.controllers.js
+++ controllers/articles.controllers.js
@@ -13,16 +13,16 @@ const getArticles = (req, res, next) => {
     });
 };
 
-const getCommentsById = (req, res, next) => {
-  const { article_id } = req.params;
-  selectCommentsById(article_id)
-    .then((comments) => {
-      console.log(comments)
-      res.status(200).send({ comments });
-    })
-    .catch((err) => {
-      next(err);
-    });
-};
+  const getCommentsById = (req, res, next) => {
+    const { article_id } = req.params;
+    selectCommentsById(article_id)
+      .then((comments) => {
+        console.log(comments)
+        res.status(200).send({ comments });
+      })
+      .catch((err) => {
+        next(err);
+      });
+  };
 
 module.exports = { getArticles, getCommentsById };
--- models/articles.models.js
+++ models/articles.models.js
@@ -20,27 +20,27 @@ const selectArticles = () => {
     });
 };
 
-const selectCommentsById = (article_id) => {
-  if (isNaN(article_id)) {
-    return Promise.reject({ status: 400, msg: "Invalid request" });
-  }
+  const selectCommentsById = (article_id) => {
+    if (isNaN(article_id)) {
+      return Promise.reject({ status: 400, msg: "Invalid request" });
+    }
 
-  return db
-    .query(
-      `
-      SELECT * FROM comments
-      WHERE article_id=$1
-      ORDER BY created_at DESC
-      `,
-      [article_id]
-    )
-    .then((response) => {
-      if (!response.rowCount) {
-        return Promise.reject({ status: 404, msg: "No results found" });
-      } else {
-        return response.rows;
-      }
-    });
-};
+    return db
+      .query(
+        `
+          SELECT * FROM comments
+          WHERE article_id=$1
+          ORDER BY created_at DESC
+          `,
+        [article_id]
+      )
+      .then((response) => {
+        if (!response.rowCount) {
+          return Promise.reject({ status: 404, msg: "No results found" });
+        } else {
+          return response.rows;
+        }
+      });
+  };
 
 module.exports = { selectArticles, selectCommentsById };

8cbd5a88a165d180dd7abc6c20c21d7b766ba633
Wes <wfreemansmith@yahoo.co.uk>
2023-02-23 13:11:40 +0000

    catch block added to user controllers

--- controllers/users.controllers.js
+++ controllers/users.controllers.js
@@ -3,6 +3,8 @@ const { selectUsers } = require("../models/users.models");
 const getUsers = (req, res, next) => {
   selectUsers().then((users) => {
     res.status(200).send({users})
+  }).catch((err) => {
+    next(err)
   });
 };
 

3da334a5b169c2e2980dffb0361767db18ea6892
Wes <wfreemansmith@yahoo.co.uk>
2023-02-23 14:13:53 +0000

    comment count added to /api/article/:article_id

--- __tests__/app.tests.js
+++ __tests__/app.tests.js
@@ -130,25 +130,16 @@ describe("app", () => {
           .expect(200)
           .then(({ body }) => {
             const { article } = body;
-            expect(article).toHaveProperty("article_id", 1);
-            expect(article).toHaveProperty(
-              "title",
-              "Living in the shadow of a great man"
-            );
-            expect(article).toHaveProperty("author", "butter_bridge");
-            expect(article).toHaveProperty(
-              "body",
-              "I find this existence challenging"
-            );
-            expect(article).toHaveProperty(
-              "created_at",
-              "2020-07-09T20:11:00.000Z"
-            );
-            expect(article).toHaveProperty("votes", 100);
-            expect(article).toHaveProperty(
-              "article_img_url",
+            expect(article.article_id).toBe(1);
+            expect(article.title).toBe("Living in the shadow of a great man");
+            expect(article.author).toBe("butter_bridge");
+            expect(article.body).toBe("I find this existence challenging");
+            expect(article.created_at).toBe("2020-07-09T20:11:00.000Z");
+            expect(article.votes).toBe(100);
+            expect(article.article_img_url).toBe(
               "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700"
             );
+            expect(article.comment_count).toBe(11);
           });
       });
       test("200 PATCH: should increment / decrement an articles vote count by given number and respond with updated article", () => {
@@ -170,17 +161,20 @@ describe("app", () => {
           });
       });
     });
-    describe('/api/users', () => {
-      test('200 GET: should return an array of users', () => {
-        return request(app).get("/api/users").expect(200).then(({body}) => {
-          const { users} = body;
-          users.forEach((user) => {
-            expect(user).toHaveProperty("username", expect.any(String));
-            expect(user).toHaveProperty("name", expect.any(String));
-            expect(user).toHaveProperty("avatar_url", expect.any(String));
-          })
-          expect(users.length).toBe(4)
-        })
+    describe("/api/users", () => {
+      test("200 GET: should return an array of users", () => {
+        return request(app)
+          .get("/api/users")
+          .expect(200)
+          .then(({ body }) => {
+            const { users } = body;
+            users.forEach((user) => {
+              expect(user).toHaveProperty("username", expect.any(String));
+              expect(user).toHaveProperty("name", expect.any(String));
+              expect(user).toHaveProperty("avatar_url", expect.any(String));
+            });
+            expect(users.length).toBe(4);
+          });
       });
     });
   });
--- models/articles.models.js
+++ models/articles.models.js
@@ -46,15 +46,23 @@ const selectArticleById = (article_id) => {
   }
   return db
     .query(
-      `SELECT * FROM articles
-    WHERE article_id=$1`,
+      `
+      SELECT articles.*,
+      COUNT(comments.article_id) AS comment_count FROM articles
+      LEFT JOIN comments
+      ON articles.article_id = comments.article_id
+      WHERE articles.article_id=$1
+      GROUP BY articles.article_id;
+      `,
       [article_id]
     )
-    .then((response) => {
-      if (response.rowCount === 0) {
+    .then(({rows}) => {
+      if (rows.length === 0) {
         return Promise.reject({ status: 404, msg: "No results found" });
       } else {
-        return response.rows[0];
+        const result = rows[0]
+        result.comment_count *= 1;
+        return result;
       }
     });
 };

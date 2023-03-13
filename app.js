95fe933ffdcdff1c7f6fd81c38b271278a4e6f25
Wes <wfreemansmith@yahoo.co.uk>
2023-03-13 11:02:51 +0000

    adding cors

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const cors = require('cors')
 
 const topicRouter = require("./routes/topics.router");
 const userRouter = require("./routes/users.router");
@@ -14,6 +15,7 @@ const {
 } = require("./controllers/error-handling.controllers.js");
 
 const app = express();
+app.use(cors())
 app.use(express.json());
 
 app.use("/api/topics", topicRouter);
